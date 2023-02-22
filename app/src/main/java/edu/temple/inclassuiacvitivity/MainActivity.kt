b3d610bd25e7f232a5f72467c16076e9df3f4141
Jordan Li <jueli@andrew.cmu.edu>
2023-02-22 10:32:30 +0900

    Finished project

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import android.widget.*
 
@@ -16,11 +17,17 @@ class MainActivity : AppCompatActivity() {
 
         // Step 1: Populate array
         val numberArray = IntArray(100)
+        var arrayTemp = 1
+        for (index in 0..99) {
+            numberArray[index] = arrayTemp
+            arrayTemp++
+        }
 
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numberArray.asList())
 
 
         // Step 2: Save selected text size
+        var textSize = 1
         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -28,7 +35,7 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-
+                textSize = numberArray[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -36,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         // Step 3: Change TextView to saved text size
         changeButton.setOnClickListener {
-
+            displayTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize.toFloat())
         }
 
     }
